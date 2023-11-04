ece666187771c036392b3a2628073cdd6a28477e
Jahirul83 <jahirulislamcse83.com>
2023-11-04 06:21:15 +0600

    booking delete operation

--- index.js
+++ index.js
@@ -78,6 +78,13 @@ async function run() {
 
     });
 
+    app.delete('/bookings/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+    })
+
 
 
     // Send a ping to confirm a successful connection
