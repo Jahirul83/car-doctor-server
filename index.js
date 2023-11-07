c6b890215e7626941f12936a4544552726e1ac47
Jahirul83 <jahirulislamcse83.com>
2023-11-07 22:13:36 +0600

    vercel deploy

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const port = process.env.PORT || 3000;
 
 // middleWare
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: [
+    // 'http://localhost:5173', 'http://localhost:5174'
+    'https://cars-doctor-64599.web.app',
+    'https://cars-doctor-64599.firebaseapp.com'
+  ],
   credentials: true
 }));
 app.use(express.json());
@@ -23,12 +27,12 @@ app.get('/', (req, res) => {
 
 
 // my middleware
-const logger = async (req, res, next) => {
+/* const logger = async (req, res, next) => {
   console.log('called', req.host, req.originalUrl);
   next();
-}
+} */
 
-const verifyToken = async (req, res, next) => {
+/* const verifyToken = async (req, res, next) => {
   const token = req.cookies?.token;
   console.log("value of token in middleware", token)
   if (!token) {
@@ -48,7 +52,7 @@ const verifyToken = async (req, res, next) => {
     next()
 
   })
-}
+} */
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i3hf6sp.mongodb.net/?retryWrites=true&w=majority`;
@@ -62,6 +66,31 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+// middleware
+
+const logger = (req, res, next) => {
+  console.log("log: info: ", req.method, req.url);
+  next();
+}
+
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  // console.log("token in the middleware: ", token);
+  // no token available
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized access' })
+  }
+  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+    if(err){
+      return res.status(401).send({message: 'unauthorized access'})
+    }
+    req.user = decoded;
+    next();
+  })
+  // next();
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -71,18 +100,39 @@ async function run() {
     const bookingCollection = client.db('carDoctor').collection('bookings');
 
     // auth related api
-    app.post('/jwt', logger, async (req, res) => {
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      // console.log('user for token', user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+
+      res.cookie('token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none'
+      })
+        .send({ success: true });
+    })
+
+    app.post('/logout', async (req, res) => {
+      const user = req.body;
+      console.log("logOut", user)
+      res.clearCookie('token', { maxAge: 0 })
+        .send({ success: true })
+    })
+
+
+
+    /* app.post('/jwt', logger, async (req, res) => {
       const user = req.body;
       console.log(user);
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
 
-      res
-        .cookie('token', token, {
-          httpOnly: true,
-          secure: false,
-        })
+      res.cookie('token', token, {
+        httpOnly: true,
+        secure: false,
+      })
         .send({ success: true });
-    })
+    }) */
 
 
     // services related api
@@ -109,14 +159,16 @@ async function run() {
 
     //bookings
     app.get('/bookings', logger, verifyToken, async (req, res) => {
-      console.log(req.query.email);
+
+      // console.log(req.query.email);
+      // console.log("coooscacas", req.cookies);
       // console.log("my token=", req.cookies.token);
       console.log("user in the valid token", req.user);
 
-      if (req.query.email !== req.user.email){
-        return res.status(403).send({message:"forbidden access"});
+      if (req.query.email !== req.user.email) {
+        return res.status(403).send({ message: "forbidden access" });
       }
-        let query = {};
+      let query = {};
       if (req.query?.email) {
         query = {
           email: req.query.email
